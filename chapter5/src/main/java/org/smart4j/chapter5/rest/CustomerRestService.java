bb091d32ff4a9d38905f57b54b09c0738b32cb9a
yufeialex <yufeialex@163.com>
2017-09-26 22:57:37 +0800

    init web framework project

---
+++ chapter5/src/main/java/org/smart4j/chapter5/rest/CustomerRestService.java
@@ -0,0 +1,32 @@
+package org.smart4j.chapter5.rest;
+
+import org.smart4j.chapter5.model.Customer;
+import org.smart4j.chapter5.service.CustomerService;
+import org.smart4j.framework.annotation.Inject;
+import org.smart4j.framework.annotation.Service;
+import org.smart4j.plugin.rest.Rest;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * 客户 REST 服务
+ *
+ * @author huangyong
+ * @since 1.0.0
+ */
+@Rest
+@Service
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class CustomerRestService {
+
+    @Inject
+    private CustomerService customerService;
+
+    @GET
+    @Path("/customer/{id}")
+    public Customer getCustomer(@PathParam("id") long customerId) {
+        return customerService.getCustomer(customerId);
+    }
+}
